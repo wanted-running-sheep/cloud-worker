0770d01db9af31c81cca4bf8d3759c503fb8897b
CHOI <tlacjd665@gmail.com>
2022-07-21 16:57:15 +0900

    ✨ feat: #3 api 모둘 구현

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useRegionModel from '@/api/models/useRegion';
 
 const ApplyPage = () => {
+  const { region, getRegionData } = useRegionModel();
+
+  useEffect(() => {
+    getRegionData();
+  }, []);
+
+  console.log(region);
   return <div>ApplyPage</div>;
 };
 
