1047515c68a0a331a9bba4fcf31af0991a22c099
Jo Hyeonho <84497426+hajun2@users.noreply.github.com>
2022-07-22 10:07:30 +0900

    ♻️ refactor: Title 컴포넌트 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import { LeftDirection, Close } from '@/assets/icons';
 
-const Title = () => {
+interface TitleProps {
+  title: string;
+  icon: 'close' | 'arrow';
+}
+
+const Title = ({ title, icon }: TitleProps) => {
+  icon === 'close' ? 'Close' : 'LeftDirection';
   return (
-    <div>
-      title wrapper
-      <Close />
-      <div>Title</div>
-    </div>
+    <TitleWrapper>
+      <div>{icon === 'close' ? <Close /> : <LeftDirection />}</div>
+      <h2>{title}</h2>
+    </TitleWrapper>
   );
 };
 
 export default Title;
 
-const TitleWrapper = styled.div``;
+const TitleWrapper = styled.div`
+  width: 100%;
+  padding: 0.8rem 0.5rem;
+  position: relative;
+  ${({ theme }) => theme.mixins.flexBox('center', 'center')};
+  div {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  h2 {
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
+`;
