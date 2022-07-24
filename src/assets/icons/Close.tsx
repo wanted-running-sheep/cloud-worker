733b561f4186f1ec9ab1d534228c90777273b03c
hajun2 <jpd93@naver.com>
2022-07-24 17:52:11 +0900

    ♻️ refactor: svg stroke 속성 수정

--- src/assets/icons/Close.tsx
+++ src/assets/icons/Close.tsx
@@ -9,11 +9,10 @@ const Close = () => {
       height="1.5rem"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={theme.color.font.gray}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      color={theme.color.font.gray}
     >
       <line x1="18" y1="6" x2="6" y2="18" />
       <line x1="6" y1="6" x2="18" y2="18" />
