cc9ccc45804c35598eaff4a91ae5c889757fdcbe
Jo Hyeonho <84497426+hajun2@users.noreply.github.com>
2022-07-26 01:46:57 +0900

    💄 style: radio 스타일 + UI 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as Close } from './Close';
 export { default as LeftDirection } from './LeftDirection';
+export { default as Checked } from './Checked';
--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Checked } from '@/assets/icons';
 
 interface RadioProps {
   name: string; //버튼 그룹. ex)성별 gender
-  label: string; //표면적 이름 ex)남자 or 여자
-  value: string; //데이터값 ex) male or female
-  checked?: string; //
+  label: string[]; //표면적 이름 ex)남자 or 여자
+  value: string[]; //데이터값 ex) male or female
 }
 
-//TODO RadioSection으로 Radio들을 감싸서 한 번에 name과 input 들을 쓸 수 있도록
-//TODO 체크 이미지 삽입
-//TODO 데이터 넘기기
-
-const Radio = ({ name, label, value, checked }: RadioProps) => {
+const Radio = ({ name, label, value }: RadioProps) => {
   const [selected, setSelected] = useState<String>();
 
   const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,53 +16,54 @@ const Radio = ({ name, label, value, checked }: RadioProps) => {
   };
 
   return (
-    <>
-      <input
-        type="radio"
-        name={name}
-        id={value}
-        value={value}
-        // checked={checked === value}
-        onChange={radioHandler}
-      />
-      <label htmlFor={value}>{label}</label>
-    </>
+    <Wrapper>
+      {label.map((label, index) => (
+        <RadioGroup key={index}>
+          <input
+            type="radio"
+            name={name}
+            id={value[index]}
+            value={value[index]}
+            onChange={radioHandler}
+          />
+          <label htmlFor={value[index]}>
+            <Checked />
+            {label}
+          </label>
+        </RadioGroup>
+      ))}
+    </Wrapper>
   );
 };
 
 export default Radio;
 
-const RadioLabel = styled.label``;
-
-const RadioBtn = styled.input`
-  opacity: 0;
-  z-index: 1;
-  border-radius: 50%;
-  margin-right: 10px;
-  &:hover {
-    background: #bebebe;
-    &::after {
-      content: '';
-      display: block;
-      border-radius: 50%;
-      width: 12px;
-      height: 12px;
-      margin: 5px;
-      background: #ffffff;
+const Wrapper = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('center', 'flex-start')};
+  width: 100%;
+  margin-top: 8px;
+  padding: 10px;
+`;
+const RadioGroup = styled.div`
+  flex: 1;
+  input {
+    display: none;
+    &:checked + label {
+      color: ${({ theme }) => theme.color.font.black};
+    }
+    &:checked + label svg {
+      fill: ${({ theme }) => theme.color.font.black};
     }
   }
-  &:checked {
-    background: #000000;
-    border: 1px solid #000000;
-    &::after {
-      content: '';
-      display: block;
-      border-radius: 50%;
-      width: 12px;
-      height: 12px;
-      margin: 5px;
-      box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.1);
-      background: white;
-    }
+  label {
+    cursor: pointer;
+    ${({ theme }) => theme.mixins.flexBox('center', 'flex-start')};
+    /* width: 80px; */
+    color: ${({ theme }) => theme.color.font.gray};
+  }
+  svg {
+    width: 1.3rem;
+    margin-right: 10px;
+    fill: ${({ theme }) => theme.color.font.gray};
   }
 `;
