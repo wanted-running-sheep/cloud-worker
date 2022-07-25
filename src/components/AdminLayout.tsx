6ac67a3cbbefdbeaca182e7dc61a1d9687f2ca81
jungjaechoi <joong8812@gmail.com>
2022-07-25 14:28:09 +0900

    💄 style: #26 각 영역의 높이를 헤더 높이 기준으로 잡도록 변경

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -21,21 +21,24 @@ export default AdminLayout;
 const Header = styled.header`
   background-color: ${({ theme }) => theme.color.background.black};
   color: ${({ theme }) => theme.color.font.white};
-  padding: 10px 14px;
+  padding-left: 15px;
+  height: 45px;
+  line-height: 45px;
 `;
 
 const Main = styled.main`
   ${({ theme }) => theme.mixins.flexBox('start', 'left')};
+  height: calc(100% - 45px);
 `;
 
-const Aside = styled.header`
+const Aside = styled.aside`
   background-color: ${({ theme }) => theme.color.background.gray};
   min-width: 160px;
-  height: 100vh;
+  height: 100%;
 `;
 
 const Section = styled.section`
   width: 100%;
-  height: 100vh;
-  padding: 60px 30px 30px 30px;
+  height: 100%;
+  padding: 60px 30px 7px 30px;
 `;
