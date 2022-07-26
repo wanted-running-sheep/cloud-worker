5f15cecfe36839d8511be39bb547f376d0a80eb9
jungjaechoi <joong8812@gmail.com>
2022-07-26 11:18:36 +0900

    ♻️ refactor: #32 엑셀 파일 다운로드 기본 파일명 수정

--- src/utils/excelDownload.ts
+++ src/utils/excelDownload.ts
@@ -1,7 +1,7 @@
 const INDEX_OF_REGION = 4;
 const INDEX_OF_PHONE = 5;
 const INDEX_OF_TRANSPORTATION = 7;
-const DOWNLOAD_FILE_NAME = 'application_status.csv';
+const DOWNLOAD_FILE_NAME_PREFIX = 'apply_user_';
 
 const formatProperDataForCsv = <T>(info: T) => {
   return Object.values(info)
@@ -33,12 +33,36 @@ const makeCSVFormat = <T>(data: T[]): string => {
   return csvRows.join('\n');
 };
 
+const padTo2Digit = (num: number) => {
+  return num.toString().padStart(2, '0');
+};
+
+const formatDate = (date: Date) => {
+  return (
+    [
+      date.getFullYear(),
+      padTo2Digit(date.getMonth() + 1),
+      padTo2Digit(date.getDate()),
+    ].join('') +
+    '_' +
+    [
+      padTo2Digit(date.getHours()),
+      padTo2Digit(date.getMinutes()),
+      padTo2Digit(date.getSeconds()),
+    ].join('')
+  );
+};
+
+const getDefaultDownloadFileName = () => {
+  return `${DOWNLOAD_FILE_NAME_PREFIX}${formatDate(new Date())}`;
+};
+
 export const downloadCSV = <T>(data: T[]) => {
   const csvData = makeCSVFormat(data);
   const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
   const url = window.URL.createObjectURL(blob);
   const aElement = document.createElement('a');
   aElement.setAttribute('href', url);
-  aElement.setAttribute('download', DOWNLOAD_FILE_NAME);
+  aElement.setAttribute('download', getDefaultDownloadFileName());
   aElement.click();
 };
