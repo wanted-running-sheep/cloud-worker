e5efb3aa22f3566d25e77b055744bd8d8b97839d
Jo Hyeonho <84497426+hajun2@users.noreply.github.com>
2022-07-27 17:46:59 +0900

    ✨ feat: 배포 파일 추가

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { HttpRequest } from '@/api/http/httpRequest';
 
-const baseURL = 'http://localhost:8000';
+const baseURL =
+  process.env.REACT_APP_MODE === 'DEV'
+    ? 'http://localhost:8000'
+    : 'https://cloud-worker-sheep.herokuapp.com';
 
 const axiosInstance = axios.create({
   baseURL,
